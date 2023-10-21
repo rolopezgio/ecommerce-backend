37f0420dce3c4a8c1a1f407b0cf07a8eb8a459ff
rolopezgio <rolopezgio@gmail.com>
2023-10-21 13:24:10 -0300

    Agregue fs

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
+const fs=require('fs')
+
 class ProductManager {
 
-    constructor() {
-        this.products = []
-    }
+    constructor(rutaDeArchivos) {
 
-    getProducts() {
-        return this.products
+        this.path=rutaDeArchivos
+        this.products = []
     }
 
     addProducts(title, description, price, thumbnail, code, stock) {
 
+        let productos = this.getProducts()
+
         let id = 1
-        if (this.products.length > 0) {
-            id = this.products[this.products.length - 1].id + 1
+        if (productos.length > 0) {
+            id = productos[productos.length - 1].id + 1
         }
 
-        let existe = this.products.find(product => product.code === code)
+        let existe = productos.find(product => product.code === code)
 
         if (!title || !description || !price || !thumbnail || stock === undefined) {
             console.log('Faltan rellenar campos');
@@ -37,29 +39,88 @@ class ProductManager {
             id
         }
 
-        this.products.push(nuevoProducto)
+        productos.push(nuevoProducto)
+
+        fs.writeFileSync(this.path, JSON.stringify(productos, null, 5))
+    }
+
+    getProducts() {
+
+        if (fs.existsSync(this.path)) {
+        return JSON.parse(fs.readFileSync(this.path, "utf-8"))
+        } else {
+            return []
+        }
     }
 
     getProductsById(id) {
 
-        let existeId = this.products.find(product => product.id === id)
+        let productos = this.getProducts();
+
+        let existeId = productos.find(product => product.id === id)
 
         if (existeId) {
             console.log(`Producto ${id} encontrado.`)
             return
         } else {
-            console.log("Not found")
+            console.log("Not found");
+            return
+        } 
+    }
+
+    updateProduct(id, objeto){
+        let productos = this.getProducts()
+
+        let indice= productos.findIndex(product=>product.id===id)
+
+        if(!indice === -1){ 
+           console.log(`El producto con id ${id} no existe`)           
+           return
+
         }
+
+        //validacion 
+
+        productos[indice]= {
+            ...productos[indice],
+            ...objeto,
+            id
+        }
+        
+        fs.writeFileSync(this.path, JSON.stringify(productos, null, 5))      
+    }
+
+    deleteProduct(id){
+         let productos = this.getProducts()
+
+         let indice= productos.findIndex(product=>product.id===id)
+
+         if(!indice === -1){ 
+            console.log(`El producto con id ${id} no existe`)           
+            return
+
+         }
+
+         productos.splice(indice, 1)
+
+         fs.writeFileSync(this.path, JSON.stringify(productos, null, 5))
     }
 }
 
-const productManager = new ProductManager()
+const productManager = new ProductManager('./archivos/productos.json')
 
 productManager.addProducts('Remera', 'Tela de algodon', 2000, 'Ruta de la imagen', 123, 10)
 productManager.addProducts('Buzo', 'Tela de hilo', 1000, 'Ruta', 123, 20)
 productManager.addProducts('Pantalon', 'Tela de nylon', 1000, 'Ruta', 124, 20)
+productManager.addProducts('Short', 'Tela de nylon', 1000, 'Ruta', 125, 20)
+productManager.addProducts('Medias', 'Tela de algodon', 1000, 'Ruta', 126, 20)
 
 console.log(productManager.getProducts())
 
-productManager.getProductsById(2)
-productManager.getProductsById(4)
+productManager.deleteProduct(3)
+
+productManager.updateProduct(1, {title:"Remeron"})
+console.log(productManager.getProducts())
+
+productManager.getProductsById(1)
+productManager.getProductsById(8)
