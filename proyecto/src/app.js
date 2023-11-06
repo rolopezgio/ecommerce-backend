87854785c5dffb1b9612b37e37f3a6b99f9df68d
rolopezgio <rolopezgio@gmail.com>
2023-11-06 19:44:27 -0300

    complete products

--- proyecto/src/app.js
+++ proyecto/src/app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const productsRouter = require('./routes/products.router')
-const cartsRouter = require('./routes/carts.router')
+// const cartsRouter = require('./routes/carts.router')
 
 const PORTO = 8080
 const app = express()
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 app.use('/api/products', productsRouter)
-app.use('/api/carts', cartsRouter)
+// app.use('/api/carts', cartsRouter)
 
 app.get('/', (req, res) => {
     res.setHeader('Content-Type', 'text/plain');
@@ -19,5 +19,3 @@ app.get('/', (req, res) => {
 const server = app.listen(PORTO, () => {
     console.log(`Server online ${PORTO}`)
 })
-
-module.exports = router;
--- proyecto/src/routes/products.router.js
+++ proyecto/src/routes/products.router.js
@@ -1,62 +1,130 @@
-const ProductManager = require ('../managers/ProductManager');
-const path=require('path');
+const ProductManager = require('../managers/ProductManager');
+const path = require('path');
 
-const Router=require('express').Router;
-const router=Router()
+const Router = require('express').Router;
+const router = Router()
 
-const productManager=new ProductManager(path.join(__dirname,"..","archivos","productos.json"))
+const productManager = new ProductManager(path.join(__dirname, "..", "archivos", "productos.json"))
 
 router.get('/', (req, res) => {
     let limit = req.query.limit
     let productos = productManager.getProducts()
     if (limit) {
-        productos=productos.slice(0, limit)
+        productos = productos.slice(0, limit)
     }
     res.json(productos)
 })
 
-router.post('/', (req,res)=>{
-  let {title, description, price, thumbnail, code, stock}=req.body
-  console.log("Recibido desde el body:",{title, description, price, thumbnail, code, stock})
+router.get('/:id', (req, res) => {
+    const productId = parseInt(req.params.id);
+    const productos = productManager.getProducts();
+    const producto = productos.find(producto => producto.id === productId);
+    if (!producto) {
+        return res.status(404).json({ mensaje: 'Producto no encontrado' });
+    }
+    res.json(producto);
+});
 
-  // validar lo obligatorio
+router.post('/', (req, res) => {
+    let { title, description, price, thumbnail, code, stock, category } = req.body
+    console.log("Recibido desde el body:", { title, description, price, thumbnail, code, stock, category })
 
-  // validar que no se repita el CODE
-  let productos = productManager.getProducts()  
+    if (!title || !description || !price || !category || stock === undefined) {
+        res.setHeader('Content-Type', 'application/json');
+        return res.status(400).json({ error: 'Faltan completar campos' });
+    }
+    let productos = productManager.getProducts()
+    let existe = productos.find(product => product.code === code)
+    if (existe) {
+        res.setHeader('Content-Type', 'application/json');
+        return res.status(400).json({ error: `El código ${code} ya existe en BD` })
+    }
+    let id = 1
+    if (productos.length > 0) {
+        id = productos[productos.length - 1].id + 1
+    }
+    let nuevoProducto = {
+        title,
+        description,
+        price,
+        thumbnail,
+        code,
+        stock,
+        category,
+        id
+    }
+    productos.push(nuevoProducto)
 
-  let existe = productos.find(product => product.code === code)
+    res.send(nuevoProducto)
 
-  if (!title || !description || !price || !thumbnail || stock === undefined) {
-      console.log('Faltan rellenar campos');
-      return;
-  }
+});
 
-  if (existe) {
-      console.log(`El código ${code} ya existe.`)
-      return
-  }
+router.put('/:id', (req, res) => {
+    let { id } = req.params
+    id = parseInt(id)
 
-  // generar el ID para el nuevo elemento
+    if (isNaN(id)) {
+        res.setHeader('Content-Type', 'application/json');
+        return res.status(400).json({ error: 'Indique un id numérico' });
+    }
+    let productos = productManager.getProducts();
 
-  let id = 1
-  if (productos.length > 0) {
-      id = productos[productos.length - 1].id + 1
-  }  
+    let indiceProducto = productos.findIndex(producto => producto.id === id);
 
-  // adecuar la respuesta
-  res.send('alta productos... completar...!!!')
+    if (indiceProducto === -1) {
+        res.setHeader('Content-Type', 'application/json');
+        return res.status(400).json({ error: `No existen usuarios con id ${id}` })
+    }
 
-}) 
+    let propiedadesPermitidas = ["title", "description", "price", "code", "stock", "category"]
+    let propiedadesQueLlegan = Object.keys(req.body)
+    let valido = propiedadesQueLlegan.every(propiedad => propiedadesPermitidas.includes(propiedad))
+    if (!valido) {
+        res.setHeader('Content-Type', 'application/json');
+        return res.status(400).json({ error: `No se aceptan algunas propiedades`, propiedadesPermitidas })
+    }
 
-// router.get('/api/products/:pid', (req, res) => {
-//     const productId = parseInt(req.params.pid);
-//     const productos = productManager.getProducts();
-//     const producto = productos.find(producto => producto.id === productId);
-  
-//     if (!producto) {
-//       return res.status(404).json({ mensaje: 'Producto no encontrado' });
-//     }  
-//     res.json(producto);
-//   });
-  
-module.exports=router
+    let productoModificado = {
+        ...productos[indiceProducto],
+        ...req.body,
+        id
+    }
+    productos[indiceProducto] = productoModificado
+
+    res.send(productos)
+
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json({
+        productoModificado
+    }); 
+});
+
+router.delete('/:id', (req, res) => {
+    let { id } = req.params
+    id = parseInt(id)
+    if (isNaN(id)) {
+        res.setHeader('Content-Type', 'application/json');
+        return res.status(400).json({ error: `Indique un id numérico` })
+    }
+
+    // NO FUNCIONA
+//     let productos = productManager.getUsuarios()
+//     let indiceProducto = productos.findIndex(producto => producto.id === id)
+//     if (indiceProducto ===-1) {
+//         res.setHeader('Content-Type', 'application/json');
+//         return res.status(400).json({ error: `No existen productos con id ${id}` })
+//     }   
+    
+    // NO FUNCIONA
+//     let productoEliminado = productos.splice(indiceProducto, 1)
+//     res.send(productos)
+//     res.setHeader('Content-Type', 'application/json');
+//     res.status(200).json({
+//         productoEliminado
+//     });
+});
+
+
+
+
+module.exports = router
